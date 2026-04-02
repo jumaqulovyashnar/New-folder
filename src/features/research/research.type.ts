d3518384accf0184c8388b48d007e3552b737e4c
yashnar <jumaqulovyashnar@gmail.com>
2026-04-02 10:54:06 +0500

    add

--- src/features/research/research.type.ts
+++ src/features/research/research.type.ts
@@ -1,3 +1,4 @@
+// GET by ID
 export type ResearchItem = {
   id: number;
   name: string;
@@ -10,6 +11,7 @@ export type ResearchItem = {
   finished: boolean;
   memberEnum: "MILLIY" | "XALQARO";
 }
+
 export interface GetByIdResponse {
   success: boolean;
   message: string;
@@ -22,9 +24,8 @@ export interface GetByIdResponse {
   };
 }
 
-// post uchun
-
-export interface CreateMemberRequest {
+// POST
+export interface CreateResearchRequest {
   name: string;
   description: string;
   year: number;          
@@ -36,9 +37,68 @@ export interface CreateMemberRequest {
   memberEnum: "MILLIY" | "XALQARO"; 
 }
 
+export interface CreateResearchResponse{
+  success: boolean;
+  message: string;
+  data: string;
+}
 
-export interface CreateMemeberResponse{
-  success:boolean,
-  messsage:string,
-  data:string,
+// GET list
+export interface ResearchRequestParams {
+  page: number;
+  size: number; 
+}
+
+export interface ResearchResponseParams{
+  success: boolean;
+  message: string;
+  data: {
+    page: number;
+    size: number;
+    totalPage: number;
+    totalElements: number;
+    body: ResearchItem[];
+  };
+}
+
+// DELETE
+export interface ResearchDeleteRequest{
+  id: number;
+}
+
+export interface ResearchDeleteResponse{
+  success: boolean;
+  message: string;
+  data: string;
+}
+
+// PUT
+export interface UpdateResearchRequest {
+  id: number;
+  name: string;
+  description: string;
+  year: number;
+  fileUrl: string;
+  userId: number;
+  member: boolean;
+  univerName: string;
+  finished: boolean;
+  memberEnum: "MILLIY" | "XALQARO";
+}
+
+export interface UpdateResearchResponse {
+  success: boolean;
+  message: string;
+  data: string;
+}
+
+// GET by ID (single)
+export interface ResearchIdRequest{
+  id: number;
+}
+
+export interface ResearchIdResponse{
+  success: boolean;
+  message: string;
+  data: ResearchItem;
 }
